a7938c180d5c8ed5efa55a940be367d2eb2f2c73
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-07-10 11:27:58 +0000

    mapeo fallido

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,29 +1,26 @@
-import React from "react";
+import React, { useContext, useState , useEffect} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Card } from "../component/card";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+export const Home = () =>  {
 
+	const [favorite,setFavorite] = useState([]);
+	const {store,actions} = useContext(Context);
 
-	
-	< div className="vw-60">
+	actions.getPeople();
+	console.log(store.people);
+
+	return (
+	<div className="characters">
 		<h1 className="text-danger">Characters</h1>
 		<div className="slider">
-				
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
+				{store.characters.map(item) => (<Card key={item.uid}/>)}
+			
+			
 		</div>
 	</div>
-	)
+	);
+}
 
